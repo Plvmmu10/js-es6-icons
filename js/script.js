2614749b407e0ccd8363c4bb870b0debd8a89e55
Plvmmu10 <gluca0260@gmail.com>
2023-03-24 15:55:53 +0100

    completed milestone 1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+/* 
+Milestone 1
+Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, in cui è presente il nome dell'icona e l'icona stessa.
+
+Milestone 2
+Ciascuna icona ha una proprietà "color": utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
+
+Milestone 3
+Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user). Quando l'utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
+
+*/
+
 const icons = [
     {
         name: 'cat',
@@ -112,3 +124,52 @@ const icons = [
         color: 'blue'
     }
 ];
+
+
+
+
+function drawCard(icons){
+    let cardBox = document.createElement('div');
+    cardBox.classList.add('d-flex', 'justify-content-center', 'align-items-center', 'flex-column', 'rounded-3', 'icon-box');
+
+    let cardSmallBox = document.createElement('div');
+    cardSmallBox.classList.add('py-3', 'd-flex', 'justify-content-center', 'align-items-center', 'flex-column');
+    
+
+    let cardIcon = document.createElement('i');
+    cardIcon.classList.add(`${icons.prefix + icons.family}`,`${icons.prefix + icons.name}`, 'fs-3');
+    
+    let cardName = document.createElement('p');
+    cardName.classList.add('text-uppercase', 'fs-3');
+    cardName.innerText = `${icons.name}`;
+    
+    cardSmallBox.append(cardIcon,cardName);
+    cardBox.append(cardSmallBox);
+
+    return cardBox;
+}
+
+function init(){
+    const iconsContainer = document.querySelector('.icons-container');
+    for (let i = 0; i < icons.length; i++){
+        const template = drawCard(icons[i]);
+
+        iconsContainer.append(template);
+       
+    }
+    
+    
+    
+}
+
+init();
+
+
+
+/* <div class="icon-box     ">
+        <div class="    ">
+            <i class=" fs-3"></i>
+        <p class="text-uppercase fs-3"></p>
+        </div>
+    </div>
+    */
